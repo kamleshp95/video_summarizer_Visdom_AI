1803fdcb81057589afe00167a6c0e2f0f14e6ed4
kamleshp95 <kamleshpadwal@gmail.com>
2023-08-25 15:34:23 +0530

    model train

--- video_summarizer/pipeline/__init__.py
+++ video_summarizer/pipeline/__init__.py
@@ -2,6 +2,7 @@ from video_summarizer.components.data_ingestion import DataIngestion
 from video_summarizer.components.data_validation import DataValidation
 from video_summarizer.components.data_preprocessing import DataProcessingConfig
 from video_summarizer.components.data_preprocessing import DataProcessing
+from video_summarizer.components.model_trainer import ModelTrainer
 import os
 import sys
 from dataclasses import dataclass
@@ -13,6 +14,7 @@ class TrainingPipelineConfig:
     data_ingestion_artifacts = os.path.join(artifacts_folder, "data_ingestion")
     data_processing_artifacts = os.path.join(
         artifacts_folder, "data_processing")
+    model_trainer_artifacts = os.path.join(artifacts_folder, "model_trainer")
 
 
 def create_directories(config=TrainingPipelineConfig):
@@ -27,6 +29,10 @@ def create_directories(config=TrainingPipelineConfig):
      # Check and create the data processing artifacts folder
     if not os.path.exists(config.data_processing_artifacts):
         os.makedirs(config.data_processing_artifacts)
+
+    # Check and create the model trainer artifacts folder
+    if not os.path.exists(config.model_trainer_artifacts):
+        os.makedirs(config.model_trainer_artifacts)
 
 
 def run_training_pipeline(config=TrainingPipelineConfig):
@@ -46,3 +52,9 @@ def run_training_pipeline(config=TrainingPipelineConfig):
     # create an instance of the 'DataProcessing' class and assigns variable as 'processor'.
     processor = DataProcessing(data_processing_config)
     processor.process_data()
+
+    # create an instance of the `ModelTrainer` class with the specified parameters.
+    trainer = ModelTrainer(model_checkpoint='facebook/bart-base',
+                           processed_dataset_folder=config.data_processing_artifacts,
+                           trainer_artifact_dir=config.model_trainer_artifacts)
+    trainer.train()
