95844f129a8078666398617814489ca1a71a3d34
kamleshp95 <kamleshpadwal@gmail.com>
2023-08-26 17:55:37 +0530

    web app created

--- video_summarizer/components/ video_to_subtitle.py
+++ video_summarizer/components/ video_to_subtitle.py
@@ -6,7 +6,7 @@ from dataclasses import dataclass
 import ffmpeg
 
 import whisper
-from video_summarizer.logger import logger
+from video_summarizer.logger import logging
 from video_summarizer.utils.util import write_srt
 
 
@@ -30,13 +30,15 @@ class SubtitleGenerator:
 # get_subtitles -> Output
 
     def extract_audio_from_video(self, video_paths: List[str]) -> Dict[str, str]:
-        temp_dir = tempfile.gettempdir() # tempfile Return the name of the directory used for temporary files
+        # tempfile Return the name of the directory used for temporary files
+        temp_dir = tempfile.gettempdir()
         audio_paths = {}
 
 # https://youtube.com/bfibufpvui
 
         for video_path in video_paths:
-            filename = os.path.basename(video_path).split(".")[0] # dot seperator
+            filename = os.path.basename(video_path).split(".")[
+                0]  # dot seperator
             output_path = os.path.join(temp_dir, f"{filename}.wav")
 
             # ffmpeg library to perform video to audio conversion
@@ -47,16 +49,16 @@ class SubtitleGenerator:
                     ac=self.audio_channels,
                     ar=self.audio_sample_rate,
                 ).run(quiet=False, overwrite_output=True, capture_stdout=True, capture_stderr=True)
-                
+
             except ffmpeg.Error as e:
                 print('stdout:', e.stdout.decode('utf8'))
                 print('stderr:', e.stderr.decode('utf8'))
                 raise e
-            
+
             audio_paths[video_path] = output_path
 
         return audio_paths
-    
+
     def generate_subtitles(
         self, audio_paths: Dict[str, str], transcribe_fn: callable
     ) -> Dict[str, str]:
@@ -65,9 +67,10 @@ class SubtitleGenerator:
         for video_path, audio_path in audio_paths.items():
             try:
                 filename = os.path.basename(video_path).split(".")[0]
-                subtitle_path = os.path.join(self.output_dir, f"{filename}.srt") # abc.srt
+                subtitle_path = os.path.join(
+                    self.output_dir, f"{filename}.srt")  # abc.srt
 
-                logger.info(
+                logging.info(
                     f"Generating subtitles for {os.path.basename(audio_path)}... This might take a while."
                 )
 
@@ -81,7 +84,8 @@ class SubtitleGenerator:
                 subtitles_paths[video_path] = subtitle_path
 
             except ffmpeg._run.Error as e:
-                logger.error(f"Error generating subtitles for {os.path.basename(audio_path)}: {str(e)}")
+                logging.error(
+                    f"Error generating subtitles for {os.path.basename(audio_path)}: {str(e)}")
 
         return subtitles_paths, result
 
@@ -97,7 +101,8 @@ class SubtitleGenerator:
         audio_paths = self.extract_audio_from_video(video_paths)
         subtitles_paths = self.generate_subtitles(
             audio_paths,
-            lambda audio_path: model.transcribe(audio_path, verbose=verbose, task=task),
+            lambda audio_path: model.transcribe(
+                audio_path, verbose=verbose, task=task),
         )
 
         return subtitles_paths
