77551a8e57656a74322c3b2202d9e208acc94558
kamleshp95 <kamleshpadwal@gmail.com>
2023-08-27 18:33:01 +0530

    resolve space issue

--- video_summarizer/components/video_downloader.py
+++ video_summarizer/components/video_downloader.py
@@ -4,7 +4,7 @@ import requests
 from pytube import YouTube
 
 from video_summarizer.exception import CustomException
-from video_summarizer.logger import logger
+from video_summarizer.logger import logging
 
 
 class VideoDownloader:
@@ -15,7 +15,7 @@ class VideoDownloader:
     def download(self) -> str | None:
         """
         It downloads a video from a given url, and returns the path to the downloaded video
-        
+
         Returns:
           The return value is a string or None.
         """
@@ -26,13 +26,13 @@ class VideoDownloader:
                 return self._download_other()
         except Exception as e:
             raise CustomException(e, sys)
-        
+
         # url = https://youtube.com
 
     def _download_youtube(self) -> str:
         """
         It downloads a youtube video from a given url and saves it to a given path
-        
+
         Returns:
           The path to the downloaded video
         """
@@ -40,25 +40,28 @@ class VideoDownloader:
             yt = YouTube(self.url)
             video = yt.streams.first()
             video.download(self.save_path)
-            logger.info(f"Youtube Video downloaded to {os.path.join(self.save_path, video.default_filename)}")
+            logging.info(
+                f"Youtube Video downloaded to {os.path.join(self.save_path, video.default_filename)}")
             return os.path.join(self.save_path, video.default_filename)
         except Exception as e:
             raise CustomException(e, sys)
-        
+
     def _download_other(self) -> str:
         """
         It downloads the video from the url and saves it to the save_path
-        
+
         Returns:
           The path to the downloaded file.
         """
         try:
             response = requests.get(self.url, stream=True)
-            filename = self.url.split("/")[-1] # https://youtube.com/ -> how to get job in data science
+            # https://youtube.com/ -> how to get job in data science
+            filename = self.url.split("/")[-1]
             with open(os.path.join(self.save_path, f"{filename}"), "wb") as f:
                 for chunk in response.iter_content(chunk_size=4096):
                     f.write(chunk)
-            logger.info(f"Video downloaded to {os.path.join(self.save_path, filename)}")
+            logging.info(
+                f"Video downloaded to {os.path.join(self.save_path, filename)}")
             return os.path.join(self.save_path, filename)
         except Exception as e:
             raise CustomException(e, sys)
